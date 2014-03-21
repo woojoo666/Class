0d3623f28e56a6b2de34872b44466004fe246a0d
woojoo666 <maxithewoowoo@gmail.com>
2014-03-20 19:22:44 -0700

    Initial Commit Heavily borrowed from http://joshgertzen.com/object-oriented-super-class-method-calling-with-javascript/ Simple use of Class.extend({prototype}) to create subclass

---
+++ Class.js
@@ -0,0 +1,16 @@
+var Class = function() {};
+Class.extend = function(proto) {
+    var subclass = function() {}; //make new subclass
+    subclass.prototype = new this(); //inherit prototype
+    subclass.extend = this.extend; //copy extend function
+
+    //append prototype
+    for (var prop in proto) {
+        subclass.prototype[prop] = proto[prop];
+
+        if (proto[prop] instanceof Function && this.prototype[prop]) //if superclass has function
+            subclass.prototype[prop].uber = this.prototype[prop]; //add uber()
+    }
+
+    return subclass;
+};
