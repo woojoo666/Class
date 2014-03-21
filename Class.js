129283b731fb9c17a31df907cb2da1f4e4f8736d
woojoo666 <maxithewoowoo@gmail.com>
2014-03-20 19:31:03 -0700

    Changed constructor and function.uber() Turns out, myFunction.uber() calls the superclass function from the scope of myFunction, so 'this' won't refer to your object anymore, and the superclass function might break.
    To fix this, I changed the constructor to store 'this' in a variable
    '_this', and then makes myFunction.uber() call the superclass function
    using '_this' as its scope.

--- Class.js
+++ Class.js
@@ -1,20 +1,28 @@
 var Class = function() {};
 Class.extend = function(proto) {
-    var subclass = proto.construct || function(){}; //make new subclass
+    var subclass = function(args) { //make new subclass
+        var _this = this; //store in closure
+
+        for (var prop in this)
+            if (this[prop] instanceof Function && this.uber[prop])
+                this[prop].uber = function(args2) { //note that any call to this function will use this[prop] as its scope
+                    _this.uber[prop].call(_this, args2); //use current instance's scope instead of this[prop]'s scope
+                };
+
+        this.construct && this.construct(args); //construct() if it exists
+    };
+
     subclass.prototype = new this(); //inherit prototype
     subclass.extend = this.extend; //copy extend function
+    subclass.prototype.uber = this.prototype;
 
     //append prototype
-    for (var prop in proto) {
+    for (var prop in proto)
         subclass.prototype[prop] = proto[prop];
-
-        if (proto[prop] instanceof Function && this.prototype[prop]) //if superclass has function
-            subclass.prototype[prop].uber = this.prototype[prop]; //add uber()
-    }
 
     return subclass;
 };
 
 function uber() {
-  return arguments.callee.caller.uber();
+    return arguments.callee.caller.uber();
 }
