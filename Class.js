02f4f41a8099fe74300f592574a435d0d67f38c6
woojoo666 <maxithewoowoo@gmail.com>
2014-03-20 23:37:21 -0700

    Fixed bug in Function.uber() construction, better testClass.html
    Originally in the subclass constructor, by the time the uber function
    was called, var 'prop' would hold its last value from the loop, and
    so every uber() function would call the same property.
    To fix it, used a closure to hold the value of prop at every iteration
    of the for-in loop.
    
    Also changed testClass.html to demonstrate the constructor and uber()
    capabilities of Class.js

--- Class.js
+++ Class.js
@@ -1,13 +1,16 @@
 var Class = function() {};
 Class.extend = function(proto) {
     var subclass = function(args) { //make new subclass
-        var _this = this; //store in closure
-
-        for (var prop in this)
-            if (this[prop] instanceof Function && this.uber[prop])
-                this[prop].uber = function(args2) { //note that any call to this function will use this[prop] as its scope
-                    _this.uber[prop].call(_this, args2); //use current instance's scope instead of this[prop]'s scope
-                };
+        for (var prop in this) {
+            if (this[prop] instanceof Function && this.uber[prop]) {
+                (function(me, func) {
+                    me[prop].uber = function(args2) { //note that any call to this function will use this[prop] as its scope
+                        console.log(func);
+                        me.uber[func].call(me, args2); //use current instance's scope instead of this[prop]'s scope
+                    };
+                })(this, prop); //store 'this' in closure and property name 'prop' and also store it in closure
+            }
+        }
 
         this.construct && this.construct(args); //construct() if it exists
     };
