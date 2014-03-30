99b2c7e8d675b65c404294ff2342c557def8129a
woojoo666 <maxithewoowoo@gmail.com>
2014-03-30 04:56:28 -0700

    Fixed functions arguments
    Used apply() and Function.arguments to pass arguments to nested
    functions, construct() and uber() can now take multiple arguments

--- Class.js
+++ Class.js
@@ -1,17 +1,17 @@
 var Class = function() {};
 Class.extend = function(proto) {
-    var subclass = function(args) { //make new subclass
+    var subclass = function() { //make new subclass
         for (var prop in this) {
             if (this[prop] instanceof Function && this.uber[prop]) {
-                (function(me, func) {
-                    me[prop].uber = function(args2) { //note that any call to this function will use this[prop] as its scope
-                        me.uber[func].call(me, args2); //use current instance's scope instead of this[prop]'s scope
+                (function(me, fn) {
+                    me[prop].uber = function() { //note that any call to this function will use this[prop] as its context
+                        me.uber[fn].apply(me, arguments); //use current instance's context instead of this[prop]'s context
                     };
                 })(this, prop); //store 'this' in closure and property name 'prop' and also store it in closure
             }
         }
 
-        this.construct && this.construct(args); //construct() if it exists
+        this.construct && this.construct.apply(this, arguments); //construct() if it exists
     };
 
     function temp() {}
@@ -27,6 +27,6 @@ Class.extend = function(proto) {
     return subclass;
 };
 
-function uber(args) {
-    return arguments.callee.caller.uber(args);
+function uber() {
+    return arguments.callee.caller.uber.apply(null, arguments);
 }
